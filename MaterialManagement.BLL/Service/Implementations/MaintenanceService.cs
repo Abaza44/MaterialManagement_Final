a393a2155c3ddfe5f0cca5e4fc52422f1405c1e4
Abaza44 <abaza4_4@icloud.com>
2025-10-11 22:27:23 +0300

    Update Maintance

--- MaterialManagement.BLL/Service/Implementations/MaintenanceService.cs
+++ MaterialManagement.BLL/Service/Implementations/MaintenanceService.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using MaterialManagement.BLL.ModelVM.Expense; // <-- أضف هذا
 using MaterialManagement.BLL.ModelVM.Maintenance;
 using MaterialManagement.BLL.Service.Abstractions;
+using MaterialManagement.DAL.DB; // <-- أضف هذا
 using MaterialManagement.DAL.Entities;
 using MaterialManagement.DAL.Repo.Abstractions;
 using System.Collections.Generic;
@@ -10,12 +12,19 @@ namespace MaterialManagement.BLL.Service.Implementations
 {
     public class MaintenanceService : IMaintenanceService
     {
+        private readonly MaterialManagementContext _context;
         private readonly IMaintenanceRecordRepo _maintenanceRepo;
-        private readonly IEquipmentRepo _equipmentRepo; // نحتاجه للحصول على اسم المعدة
+        private readonly IEquipmentRepo _equipmentRepo;
         private readonly IMapper _mapper;
 
-        public MaintenanceService(IMaintenanceRecordRepo maintenanceRepo, IEquipmentRepo equipmentRepo, IMapper mapper)
+        // <<< تم تحديث الـ Constructor >>>
+        public MaintenanceService(
+            MaterialManagementContext context,
+            IMaintenanceRecordRepo maintenanceRepo,
+            IEquipmentRepo equipmentRepo,
+            IMapper mapper)
         {
+            _context = context;
             _maintenanceRepo = maintenanceRepo;
             _equipmentRepo = equipmentRepo;
             _mapper = mapper;
@@ -27,17 +36,44 @@ namespace MaterialManagement.BLL.Service.Implementations
             return _mapper.Map<IEnumerable<MaintenanceRecordViewModel>>(records);
         }
 
+        
         public async Task<MaintenanceRecordViewModel> AddMaintenanceRecordAsync(MaintenanceRecordCreateModel model)
         {
-            var record = _mapper.Map<MaintenanceRecord>(model);
-            var createdRecord = await _maintenanceRepo.CreateAsync(record);
+            using var transaction = await _context.Database.BeginTransactionAsync();
+            try
+            {
+                
+                var record = _mapper.Map<MaintenanceRecord>(model);
+                _context.MaintenanceRecords.Add(record);
 
-            // نحتاج جلب اسم المعدة لعرضه في الـ ViewModel
-            var equipment = await _equipmentRepo.GetByCodeAsync(createdRecord.EquipmentCode);
-            var viewModel = _mapper.Map<MaintenanceRecordViewModel>(createdRecord);
-            viewModel.EquipmentName = equipment?.Name ?? "غير معروف";
+                
+                if (model.Cost > 0)
+                {
+                    var equipment = await _equipmentRepo.GetByCodeAsync(model.EquipmentCode);
+                    var expense = new Expense
+                    {
+                        Description = $"صيانة للمعدة: {equipment?.Name} - {model.Description}",
+                        Amount = model.Cost,
+                        ExpenseDate = model.MaintenanceDate,
+                        Category = "صيانة",
+                        PaymentTo = model.PerformedBy,
+                        IsActive = true,
+                        CreatedDate = System.DateTime.Now
+                    };
+                    _context.Expenses.Add(expense);
+                }
 
-            return viewModel;
+                
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+
+                return _mapper.Map<MaintenanceRecordViewModel>(record);
+            }
+            catch (System.Exception)
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
         }
     }
 }
